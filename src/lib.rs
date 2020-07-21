b7bf34f999599a0dcb65008777747946f09e7545
marc2332 <mespinsanz@gmail.com>
2020-07-21 22:57:38 +0200

    :construction: Component test

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,42 @@ use orbtk::prelude::*;
 use deno_core;
 use calcite;
 
+extern crate dces;
+
+#[macro_use]
+extern crate serde_derive;
+
+#[derive(Deserialize)]
+struct Comp<'a> {
+	_type: &'a str,
+	_text:  &'a str
+}
+
+widget!(
+	Element {
+		text: String16
+	}
+);
+
+impl Template for Element {
+	fn template(self, id: Entity, ctx: &mut BuildContext) -> Self {
+		self.name("element")
+	}
+}
+
+fn createComp(child: Comp, ctx: &mut BuildContext) -> dces::entity::Entity {
+	Element::new().text(child._text).build(ctx)
+}
+
 #[calcite::deno_op]
-fn createApp(name: String) {
+fn createApp(name: String, root: Comp<'static>) {
 	Application::new()
 	.window(move |ctx| {
 		Window::new()
 		.title(String::from(&name))
 		.position((100.0, 100.0))
 		.size(400.0, 400.0)
-		.child(TextBlock::new().text("Sample").build(ctx))
+		.child(createComp(root, ctx))
 		.build(ctx)
 	})
 	.run();
